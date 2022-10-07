d94f86e0afe1eaf06c8897266f59e050b19c8caf
Vicent Botella <vb@interap.de>
2022-10-07 16:33:18 +0200

    Code to original state

--- src/Data/Entities/Camp.cs
+++ src/Data/Entities/Camp.cs
@@ -9,7 +9,6 @@ namespace CoreCodeCamp.Data
     public int CampId { get; set; }
     public string Name { get; set; }
     public string Moniker { get; set; }
-    public string Test { get; set; }
     public Location Location  { get; set; }
     public DateTime EventDate { get; set; } = DateTime.MinValue;
     public int Length { get; set; } = 1;
