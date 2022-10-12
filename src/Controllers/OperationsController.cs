86452ce7a89006b8471d275fc4b86a525cba7443
Vicent Botella <vb@interap.de>
2022-10-12 11:50:23 +0200

    operations controller created (functional API feature), first API version finished

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration config; 
+
+        public OperationsController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)this.config;
+                root.Reload();
+                return Ok("Configuration reloaded");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Couldn't reload configuration");
+            }
+        }
+    }
+}
